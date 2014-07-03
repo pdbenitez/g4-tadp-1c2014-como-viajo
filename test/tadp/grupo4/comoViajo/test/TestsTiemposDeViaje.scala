5a179eceaf70dd53dd98815e38983caa9368ee1e
martin <martoo6@gmail.com>
2014-07-03 19:50:27 -0300

    Defincion de Tests especificos para TIEMPOS DE VIAJE

--- test/tadp/grupo4/comoViajo/test/TestsTiemposDeViaje.scala
+++ test/tadp/grupo4/comoViajo/test/TestsTiemposDeViaje.scala
@@ -6,5 +6,28 @@ import org.scalatest.{Matchers, FlatSpec}
  * Created by martin on 03/07/14.
  */
 class TestsTiemposDeViaje extends FlatSpec with Matchers {
+  it should "Obtener tiempo subte SIN combinacion" in{
 
+  }
+  it should "Obtener tiempo subte CON combinacion" in{
+
+  }
+  it should "Obtener tiempo tren SIN combinacion" in{
+
+  }
+  it should "Obtener tiempo tren CON combinacion" in{
+
+  }
+  it should "Obtener tiempo SUBTE CON combinacion en TREN" in{
+
+  }
+  it should "Obtener tiempo colectivo SIN combinacion" in{
+
+  }
+  it should "Obtener tiempo colectivo SIN combinacion" in{
+
+  }
+  it should "Obtener tiempo colectivo CON combinacion (Independiente de con quien)" in{
+
+  }
 }
