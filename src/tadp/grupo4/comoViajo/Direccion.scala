70f87338d1abf3e4eec6edfc4ae94ec3d19f0198
martin <martoo6@gmail.com>
2014-07-03 08:34:51 -0300

    Agregado eq en Direccion

--- src/tadp/grupo4/comoViajo/Direccion.scala
+++ src/tadp/grupo4/comoViajo/Direccion.scala
@@ -1,6 +1,7 @@
 package tadp.grupo4.comoViajo
 
-class Direccion(val unaCalle: String,val altura: Int,val unaZona: String) {
+class Direccion(val calle: String,val altura: Int,val zona: String) {
   //var calleEsquina1: String = null
   //var calleEsquina2: String = null
+  def eq(unaDireccion: Direccion) = (unaDireccion.calle == calle) && (unaDireccion.altura == altura) && (unaDireccion.zona == zona)
 }
