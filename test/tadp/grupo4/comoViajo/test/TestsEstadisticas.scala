32ef12717f414b54ced7a93890425f58a8931f4a
martin <martoo6@gmail.com>
2014-07-03 20:02:13 -0300

    Defincion de Tests especificos para ESTADISTICAS

--- test/tadp/grupo4/comoViajo/test/TestsEstadisticas.scala
+++ test/tadp/grupo4/comoViajo/test/TestsEstadisticas.scala
@@ -6,5 +6,30 @@ import org.scalatest.{Matchers, FlatSpec}
  * Created by martin on 03/07/14.
  */
 class TestsEstadisticas extends FlatSpec with Matchers {
+//  **DATOS A OBTENER**
+//  Costo promedio de los viajes.
+//  Tiempo promedio de los viajes.
+//  Porcentaje de utilizacion (cantidad de viajes)
+//  Facturacion total.
 
+//  **FILTROS**
+//  Comparar las diferentes zonas de la ciudad entre si.
+//  Comparar las compañias de transporte entre si.
+//  Comparar los tipos de transporte entre si (tren, subte, colectivo).
+//  Comparar los medios de transporte entre si (cada una de las lineas)
+
+
+  //Se podrian/deberian agregar mas tests (Nota: esto sale como piña en funcional con Map, FlatMap, Filter, Etc..)
+  it should "Dada una zona la proporcion en que se reparten los viajes en esa zona entre trenes, subtes y colectivos." in {
+
+  }
+  it should "La facturacion total (en viajes seleccionados) para cada tipo de transporte para todo el sistema" in {
+
+  }
+  it should "El tiempo (o costo) promedio de cada tipo de transporte." in {
+
+  }
+  it should "La facturacion total de cada compañia de medios transporte." in {
+
+  }
 }
