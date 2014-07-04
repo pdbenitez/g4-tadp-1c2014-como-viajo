7a39f82118b947c8b69bae3a1e3151862b489250
martin <martoo6@gmail.com>
2014-07-04 20:40:36 -0300

    Agregado a tests, ordenado utilizando metodo java

--- src/tadp/grupo4/comoViajo/BuscadorDeViajes.scala
+++ src/tadp/grupo4/comoViajo/BuscadorDeViajes.scala
@@ -1,5 +1,8 @@
 package tadp.grupo4.comoViajo
 
+import java.util.{Comparator, Collections}
+
+import scala.collection.JavaConverters._
 import tadp.grupo4.comoViajo.CriterioBusqueda._
 import tadp.grupo4.moduloExterno.ModuloExterno
 
@@ -21,13 +24,24 @@ object BuscadorDeViajes {
         }
       }
     }
-
+    var lstViajes = viajesSinCombinacion.asJava
     criterio match{
-      case CriterioBusqueda.Costo => viajesSinCombinacion.sortBy(-_.getCosto).toList //El menos me permite ordenar de menor a mayor..je
-      case CriterioBusqueda.Tiempo => viajesSinCombinacion.sortBy(-_.getTiempo).toList
+      case CriterioBusqueda.Costo => Collections.sort(lstViajes, new Comparator[Viaje](){
+        def compare(v1 :Viaje, v2 :Viaje):Int={
+          return v1.getCosto.compareTo(v2.getCosto)
+        }
+      })
+      case CriterioBusqueda.Tiempo => Collections.sort(lstViajes, new Comparator[Viaje](){
+        def compare(v1 :Viaje, v2 :Viaje):Int={
+          return v1.getTiempo.compareTo(v2.getTiempo)
+        }
+      })
+        //Asi quedaria con funcional
+//      case CriterioBusqueda.Costo => viajesSinCombinacion.sortBy(-_.getCosto).toList //El menos me permite ordenar de menor a mayor..je
+//      case CriterioBusqueda.Tiempo => viajesSinCombinacion.sortBy(-_.getTiempo).toList
     }
 
-
+    lstViajes.asScala.toList
     //TODO: Si no hay viajes sin combinacion entonces ver los viajes posible con combinacion
     //TODO: tambien faltaria ordenar por el criterio de viaje, no se como esta pero podria ser un enum perfectamnete
     //TODO: tambien faltaria pasar la tarjeta tal y como dijo Jorge. La tarjeta puede ser un parametro opcional (Scala acepta parametros opcionales)
