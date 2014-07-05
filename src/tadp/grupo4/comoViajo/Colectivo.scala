f813970905b03e6027c9b91b20a3f46c9ee7cc47
Jorge Baez <jorgebaez212@gmail.com>
2014-07-05 04:21:50 -0300

    Correccion de empresas en constructores

--- src/tadp/grupo4/comoViajo/Colectivo.scala
+++ src/tadp/grupo4/comoViajo/Colectivo.scala
@@ -2,7 +2,7 @@ package tadp.grupo4.comoViajo
 
 import tadp.grupo4.moduloExterno.ModuloExterno
 
-class Colectivo(linea: Int, empresa:Empresa) extends Transporte {
+class Colectivo(linea: Int, var empresa:Empresa) extends Transporte {
 
   val numeroColectivo = linea
 
