a05cb6adf5a5c9559150125658f4a8c662e1fe01
Jorge Baez <jorgebaez212@gmail.com>
2014-07-05 03:04:12 -0300

    Primer commit tiempos

--- src/tadp/grupo4/comoViajo/Viaje.scala
+++ src/tadp/grupo4/comoViajo/Viaje.scala
@@ -1,5 +1,7 @@
 package tadp.grupo4.comoViajo
 
+import tadp.grupo4.moduloExterno.ModuloExterno
+
 class Viaje (var recorridos: List[Recorrido]){
 
   //var origen: Direccion
@@ -27,8 +29,26 @@ class Viaje (var recorridos: List[Recorrido]){
   def getTiempo: Double =
     {
       var tiempoTotal: Double = 0
+      var recorridoAnterior:Recorrido = null
       for(unRecorrido<- recorridos){
-        tiempoTotal+= unRecorrido.tiempo
+        if(recorridoAnterior!=null){
+          val tAnterior = recorridoAnterior.trans
+          val tActual = unRecorrido.trans
+          if(tAnterior.isInstanceOf[Subte] && tActual.isInstanceOf[Subte] && recorridoAnterior.dest.direccion.eq(unRecorrido.orig.direccion))
+            tiempoTotal+= 4
+          else if (tAnterior.isInstanceOf[Subte] && tActual.isInstanceOf[Tren] && recorridoAnterior.dest.direccion.eq(unRecorrido.orig.direccion))
+            tiempoTotal+= 5
+          else if (tAnterior.isInstanceOf[Tren] && tActual.isInstanceOf[Tren] && recorridoAnterior.dest.direccion.eq(unRecorrido.orig.direccion))
+            tiempoTotal+= 6
+          else{
+            val dirIniComb = recorridoAnterior.dest.direccion
+            val dirFinComb = unRecorrido.orig.direccion
+            tiempoTotal+= (ModuloExterno.getDistanciaAPie(dirIniComb,dirFinComb))*25  //((dist en km*1000/100)*2.5)
+          }
+        }else{
+          tiempoTotal+= unRecorrido.tiempo
+        }
+        recorridoAnterior=unRecorrido
       }
       tiempoTotal
     }
