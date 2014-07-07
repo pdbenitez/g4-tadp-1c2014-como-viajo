7ba42f2b85c753e80b21a15b9ea2ca59253cf094
martin <martoo6@gmail.com>
2014-07-07 00:46:00 -0300

    Syntactic sugar en fors anidados

--- src/tadp/grupo4/comoViajo/BuscadorDeViajes.scala
+++ src/tadp/grupo4/comoViajo/BuscadorDeViajes.scala
@@ -11,52 +11,44 @@ import scala.collection.mutable.ListBuffer
 object BuscadorDeViajes {
 
   var viajesHistoricos = new ListBuffer[Viaje]
-  def obtenerViajes(origenNombre: String,origenAltura: Long, destinoNombre: String,destinoAltura: Long, maxDistCaminar: Double = 0.1, criterio: CriterioBusqueda = CriterioBusqueda.Tiempo): List[Viaje] ={
+
+  def obtenerViajes(origenNombre: String, origenAltura: Long, destinoNombre: String, destinoAltura: Long, maxDistCaminar: Double = 0.1, criterio: CriterioBusqueda = CriterioBusqueda.Tiempo): List[Viaje] = {
 
     val paradasOrigen = ModuloExterno.buscarParadasMasCercanas(origenNombre, origenAltura, maxDistCaminar)
     val paradasDestino = ModuloExterno.buscarParadasMasCercanas(destinoNombre, destinoAltura, maxDistCaminar)
 
-    val viajesSinCombinacion= new ListBuffer[Viaje]
+    val viajesSinCombinacion = new ListBuffer[Viaje]
 
-    for(paradaOrigen<-paradasOrigen){
-      for(paradaDestino<-paradasDestino){
-        if(paradaDestino.transporte.eq(paradaOrigen.transporte)){
-          val recorridos = List(new Recorrido(paradaOrigen, paradaDestino, paradaOrigen.transporte))
-          viajesSinCombinacion+=new Viaje(recorridos)
+    for (paradaOrigen <- paradasOrigen; paradaDestino <- paradasDestino; if (paradaDestino.transporte.eq(paradaOrigen.transporte))) {
+      val recorridos = List(new Recorrido(paradaOrigen, paradaDestino, paradaOrigen.transporte))
+      viajesSinCombinacion += new Viaje(recorridos)
+    }
+
+    val viajesConCombinacion = new ListBuffer[Viaje]
+
+    for (pOrigenInicio <- paradasOrigen; pDestinoFin <- paradasDestino; if (!pOrigenInicio.transporte.eq(pDestinoFin.transporte))) {
+      for (pOrigenFin <- pOrigenInicio.transporte.listaDeParadas; paradaDestinoInicio <- pDestinoFin.transporte.listaDeParadas) {
+        if (ModuloExterno.getDistanciaAPie(pOrigenFin.direccion, paradaDestinoInicio.direccion) < maxDistCaminar) {
+          if ((!pOrigenInicio.eq(pOrigenFin)) && (!paradaDestinoInicio.eq(pDestinoFin))) {
+            val recorridos = List(new Recorrido(pOrigenInicio, pOrigenFin, pOrigenInicio.transporte), new Recorrido(paradaDestinoInicio, pDestinoFin, pDestinoFin.transporte))
+            viajesConCombinacion += new Viaje(recorridos)
+          }
         }
       }
     }
 
-    val viajesConCombinacion= new ListBuffer[Viaje]
-
-    for(pOrigenInicio<-paradasOrigen){
-    for(pDestinoFin<-paradasDestino) {
-        if(!pOrigenInicio.transporte.eq(pDestinoFin.transporte)){
-          for(pOrigenFin<-pOrigenInicio.transporte.listaDeParadas){
-          for(paradaDestinoInicio<-pDestinoFin.transporte.listaDeParadas){
-              if(ModuloExterno.getDistanciaAPie(pOrigenFin.direccion, paradaDestinoInicio.direccion)<maxDistCaminar){
-              if((!pOrigenInicio.eq(pOrigenFin)) && (!paradaDestinoInicio.eq(pDestinoFin))){
-                  val recorridos = List(new Recorrido(pOrigenInicio, pOrigenFin, pOrigenInicio.transporte),new Recorrido(paradaDestinoInicio, pDestinoFin, pDestinoFin.transporte))
-                  viajesConCombinacion+=new Viaje(recorridos)
-              }
-              }
-          }
-          }
-        }
-    }
-    }
-
-    val lstViajes = viajesSinCombinacion++viajesConCombinacion
-    criterio match{
-      case CriterioBusqueda.Costo => lstViajes.sortBy(- _.getCosto()).toList //El menos me permite ordenar de menor a mayor..je
-      case CriterioBusqueda.Tiempo => lstViajes.sortBy(- _.getTiempo).toList
+    val lstViajes = viajesSinCombinacion ++ viajesConCombinacion
+    criterio match {
+      case CriterioBusqueda.Costo => lstViajes.sortBy(-_.getCosto()).toList //El menos me permite ordenar de menor a mayor..je
+      case CriterioBusqueda.Tiempo => lstViajes.sortBy(-_.getTiempo).toList
     }
   }
 
-  def confirmarViaje(unViaje : Viaje) : Unit = {
-    viajesHistoricos+=unViaje
+  def confirmarViaje(unViaje: Viaje): Unit = {
+    viajesHistoricos += unViaje
   }
-  def limpiarViajes() : Unit = {
+
+  def limpiarViajes(): Unit = {
     viajesHistoricos.clear()
   }
 
